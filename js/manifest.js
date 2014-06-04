ed996ee7ff2f679a3b4f2b038ecd06d7574e3ffa
szatkus <szatkus@gmail.com>
2014-06-04 21:36:37 +0200

    Fixed manifest parsing

--- js/manifest.js
+++ js/manifest.js
@@ -6,10 +6,14 @@
 js2me.parseManifest = function (content) {
 	var manifest = {};
 	var lines = content.split('\n');
+	var lastSection;
 	for (var i in lines) {
 		var parts = lines[i].split(':');
 		if (parts.length > 1) {
-			manifest[parts[0].toLowerCase()] = parts[1].trim();
+			lastSection = parts[0].toLowerCase();
+			manifest[lastSection] = parts[1].trim();
+		} else {
+			manifest[lastSection] += parts[0].trim();
 		}
 	}
 	return manifest;
