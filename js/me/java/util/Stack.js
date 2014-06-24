a32791bd7379ea828c6b927cf2b367fcfe3351ea
szatkus <szatkus@gmail.com>
2014-06-24 20:59:00 +0200

    push

--- js/me/java/util/Stack.js
+++ js/me/java/util/Stack.js
@@ -1,3 +1,10 @@
 js2me.createClass({
+	/*
+	 * public Object push(Object item)
+	 */
+	$push$Ljava_lang_Object_$Ljava_lang_Object_: function (obj) {
+		this.$addElement$Ljava_lang_Object_$V(obj);
+		return obj;
+	},
 	superClass: 'javaRoot.$java.$util.$Vector'
 });
