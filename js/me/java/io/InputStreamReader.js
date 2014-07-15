7f40faa157d2c4dd598cf9432137ddb942c45558
szatkus <szatkus@gmail.com>
2014-07-15 23:07:38 +0200

    Missing methods in InputStreamReader

--- js/me/java/io/InputStreamReader.js
+++ js/me/java/io/InputStreamReader.js
@@ -6,8 +6,30 @@ js2me.createClass({
 	_init$Ljava_io_InputStream_$V: function(stream) {
 		this.stream = stream;
 	},
+	/*
+	 * public InputStreamReader(InputStream is, String enc) throws UnsupportedEncodingException
+	 */
+	_init$Ljava_io_InputStream_Ljava_lang_String_$V: function (stream) {
+		// I don't care about your encoding
+		this.stream = stream;
+	},
+	/*
+	 * public void close() throws IOException
+	 */
 	$close$$V: function () {
 		this.stream.$close$$V();
+	},
+	/*
+	 * public int read(char[] cbuf) throws IOException
+	 */
+	$read$_C$I: function (output) {
+		var buffer = new Array(this.stream.$available$$I());
+		this.stream.$read$_BII$I(buffer, 0, buffer.length);
+		var str = js2me.UTF8ToString(buffer, 0, buffer.length);
+		for (var i = 0; i < str.length; i++) {
+			output[i] = str.charCodeAt(i);
+		}
+		return str.length;
 	},
 	/*
 	 * public int read()
